4a90d11dc71b987e763e17e09a8d1a806d2bfe3f
ARIJIT PARIA <72959650+arijitparia2002@users.noreply.github.com>
2021-02-18 09:47:12 +0530

    Update P 26_pointer_to_class.cpp

--- P 26_pointer_to_class.cpp
+++ P 26_pointer_to_class.cpp
@@ -4,9 +4,13 @@ using namespace std;
 class Demo{
 	public:
 		int a = 20;
+		int b;
 		
 		void demoFunc(){
-			cout<<"I am a member function.";
+			cout<<"I am a member function."<<endl;
+		}
+		void showData(){
+			cout<<"Value of b : "<<b<<endl;
 		}
 	};
 int main(){
@@ -17,4 +21,12 @@ int main(){
 	
 	cout<<"Accessing data member from class : "<<ptr->a<<endl; //accessing data member by pointer
 	ptr->demoFunc();//accessing member function by pointer
-}
+	
+	int Demo::*ptr1 = &Demo::b;
+	obj.*ptr1 = 30;
+	obj.showData();
+	
+	ptr->*ptr1 = 44;
+	ptr->showData();//using pointer
+	
+	}
