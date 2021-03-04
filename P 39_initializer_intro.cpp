3ab0a467bf2b13f76618d58a3058036e4b7eb0a0
ARIJIT PARIA <72959650+arijitparia2002@users.noreply.github.com>
2021-03-04 10:00:18 +0530

    Update P 39_initializer_intro.cpp

--- P 39_initializer_intro.cpp
+++ P 39_initializer_intro.cpp
@@ -3,10 +3,10 @@
  
  class  Demo{
  	private:
- 		int x,y,z;
+ 		const int x,y,z;//wecan initialize constant by initializer
  	public:
  		Demo(int i=0, int j=0, int k=0):x(i),y(j),z(k){}; // syntax
-		 
+		  				    //can not initialize constant with constructor
  	void showData(){
  		cout<<x<<" "<<y<<" "<<z<<endl;
 	 }
